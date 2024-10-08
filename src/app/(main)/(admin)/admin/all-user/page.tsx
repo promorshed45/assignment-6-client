ccb0a4ff12f2155391de8e35e90b29e82d618cab
promorshed45 <ctgmorshed45@gmail.com>
2024-10-08 23:53:57 +0600

    ready for production

--- src/app/(main)/(admin)/admin/all-user/page.tsx
+++ src/app/(main)/(admin)/admin/all-user/page.tsx
@@ -12,47 +12,15 @@ import {
   Image,
 } from "@nextui-org/react";
 import { AiFillDelete } from "react-icons/ai";
-import { toast } from "sonner";
-import Swal from "sweetalert2";
 
-import { TResponse, TUser } from "@/src/types";
-import {
-  useAlluserQuery,
-  useDeleteUserMutation,
-} from "@/src/redux/features/user/userApi";
-import EditUser from "@/src/components/ui/allUser/EditUser";
+import EditUser from "./_components/EditUser";
+import { IUser } from "@/src/types";
 
 export default function AllUser() {
-  const { data: allUser } = useAlluserQuery(undefined);
-  const [deleteUser] = useDeleteUserMutation();
+  
 
   const handleDeleteUser = async (id: string) => {
-    try {
-      // before delete alert
-      Swal.fire({
-        title: "Are you sure?",
-        text: "You went to delete this user",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, delete it!",
-      }).then(async (result) => {
-        if (result.isConfirmed) {
-    const toastId = toast.loading("deleting....");
-
-          // delete mutation
-          const res = (await deleteUser(id)) as TResponse<any>;
-          if (res?.data) {
-            toast.warning("Delete Success", { id: toastId, duration: 1000 });
-          } else {
-            toast.error(res?.error?.data?.message, { id: toastId });
-          }
-        }
-      });
-    } catch (error: any) {
-      toast.error(error?.message);
-    }
+    console.log(id);
   };
 
   return (
@@ -66,11 +34,11 @@ export default function AllUser() {
         <TableColumn>Action</TableColumn>
       </TableHeader>
       <TableBody>
-        {allUser?.data?.map((user: TUser) => {
+        {allUser?.data?.map((user: IUser) => {
           return (
             <TableRow key={user?._id}>
               <TableCell>
-                <Image className="size-10" src={user?.image} />
+                <Image className="size-10" src={user?.profilePhoto} />
               </TableCell>
               <TableCell>{user?.name}</TableCell>
               <TableCell>{user?.role}</TableCell>
